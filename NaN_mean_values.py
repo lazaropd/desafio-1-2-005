92824a95ba19c5bc6728687b0b025f273fc218bb
wbbatista <batista.wana@gmail.com>
2019-10-21 11:02:38 -0300

    Update NaN_mean_values.py

--- NaN_mean_values.py
+++ NaN_mean_values.py
@@ -9,9 +9,11 @@ class wana:
         df = pd.read_csv(file, header=0, sep=';') #reading my file
         for i in df: # for in dataframe's columns
             self.data_NaN[i] = np.where((df[i] == '') | (df[i]== 'Sem Informações'),1, 0) # filling my empty dataframe by conditions: where my conditions are true it sets 1 and zero when false
-        return self.data_NaN
+        self.data_mean = self.data_NaN.mean().tolist()
+        return self.data_mean
 
 linha = wana()
 data_mean = linha.read_file('portalbio_export_16-10-2019-14-39-54.csv')
-data_mean = data_mean.mean()
-print(data_mean.mean)
+
+print(data_mean)
+
